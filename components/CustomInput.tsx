19f97e057e0bffdc93fb3fbbd662ce70e99d9e0b
Jorge Alcala <58161878+jorge1alcala@users.noreply.github.com>
2024-10-13 12:34:00 -0700

    finishing form

---
+++ components/CustomInput.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { FormField, FormLabel, FormControl, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+import { Control, FieldPath } from "react-hook-form";
+import { z } from "zod";
+import { AuthFormSchema } from "@/lib/utils";
+
+const formSchema = AuthFormSchema("sign-up");
+
+interface CustomInput {
+  control: Control<z.infer<typeof formSchema>>;
+  name: FieldPath<z.infer<typeof formSchema>>;
+  label: string;
+  placeholder: string;
+}
+
+const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel className="form-label">{label}</FormLabel>
+          <div className="flex w-full flex-col">
+            <FormControl>
+              <Input
+                placeholder={placeholder}
+                className="input-class mt-2"
+                type={name === "password" ? "password" : "text"}
+                {...field}
+              />
+            </FormControl>
+            <FormMessage className="form-message" />
+          </div>
+        </div>
+      )}
+    />
+  );
+};
+
+export default CustomInput;
