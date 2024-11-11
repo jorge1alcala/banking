4cd28b9e47644d39ada4d2565113b067d7a68249
Jorge Alcala <58161878+jorge1alcala@users.noreply.github.com>
2024-11-10 17:26:35 -0800

    database conection

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -19,8 +19,11 @@ import {
 } from "@/components/ui/form";
 import CustomInput from "./CustomInput";
 import { AuthFormSchema } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { signIn, signUp } from "@/lib/actions/user.actions";
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -36,13 +39,28 @@ const AuthForm = ({ type }: { type: string }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     setIsLoading(true);
-    console.log(values);
+    try {
+      //sign up with Appwrie & create plaid token
+      if (type === "sign-up") {
+        const newUser = await signUp(data);
+        setUser(newUser);
+      }
+      if (type === "sign-in") {
+        // const response = await signIn({
+        //   email: data.email,
+        //   password: data.password
+        // });
+        // if (response) router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
     setIsLoading(false);
-  }
+  };
 
   return (
     <section className="auth-form">
@@ -97,6 +115,12 @@ const AuthForm = ({ type }: { type: string }) => {
                     name="address1"
                     label="Address"
                     placeholder="Enter your specific address"
+                  />
+                  <CustomInput
+                    control={form.control}
+                    name="city"
+                    label="City"
+                    placeholder="Enter your specific city"
                   />
                   <div className="flex gap-4">
                     <CustomInput
